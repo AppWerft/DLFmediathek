08c560f62920bc75fec8963a52ea420eed493bf0
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2015-02-16 15:03:04 +0100

         

---
+++ Resources/android/feedtester.js
@@ -0,0 +1,10 @@
+//https://gist.github.com/manumaticx/6239830
+
+var serviceIntent = Ti.Android.currentService.getIntent(); 
+mirrorPodcasts();
+Ti.Android.stopService(serviceIntent);
+
+
+function mirrorPodcasts() {
+    
+}
