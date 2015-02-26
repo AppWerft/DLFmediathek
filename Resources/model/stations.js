7f8a1087fd06d61e6447dfa903d13aa678a713b9
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2015-02-26 12:51:07 +0100

         

--- Resources/model/stations.js
+++ Resources/model/stations.js
@@ -5,21 +5,21 @@ module.exports = {
          einen Hintergrundprozess gesynct*/
         rss : 'http://www.deutschlandfunk.de/programmvorschau.281.de.rss',
         /* für LiveRadio, eigentlicher Stream wird ermittelt und gespeichert*/
-        stream : 'http://www.dradio.de/streaming/dlf.m3u',
+        livestream : 'http://www.dradio.de/streaming/dlf.m3u',
         /* wird jede Minutre aufgerufen, wenn View aktiv ist */
-        podcasts : 'http://srv.deutschlandradio.de/aodlistaudio.1706.de.rpc?drau:station_id=4&drau:from=_DATE_&drau:to=_DATE_&drau:page=1&drau:limit=500'
+        livemediathek : 'http://srv.deutschlandradio.de/aodlistaudio.1706.de.rpc?drau:station_id=4&drau:from=_DATE_&drau:to=_DATE_&drau:page=1&drau:limit=500'
     },
     drk : {
         color : '#E95D0F',
         hoerkunst : 'http://www.deutschlandradiokultur.de/hoerkunst.1656.de.html',
         rss: 'http://www.deutschlandradiokultur.de/programmvorschau.282.de.rss',
-        stream : 'http://www.dradio.de/streaming/dkultur.m3u',
-        podcasts : 'http://srv.deutschlandradio.de/aodlistaudio.1706.de.rpc?drau:station_id=3&drau:from=_DATE_&drau:to=_DATE_&drau:page=1&drau:limit=500'
+        livestream : 'http://www.dradio.de/streaming/dkultur.m3u',
+        livemediathek : 'http://srv.deutschlandradio.de/aodlistaudio.1706.de.rpc?drau:station_id=3&drau:from=_DATE_&drau:to=_DATE_&drau:page=1&drau:limit=500'
     },
     drw : {
         color : '#01953C',
-        stream : 'http://www.deutschlandradio.de/streaming/dradiowissen.m3u',
-        podcasts : 'http://srv.deutschlandradio.de/aodlistaudio.1706.de.rpc?drau:station_id=1&drau:from=_DATE_&drau:to=_DATE_&drau:page=1&drau:limit=500'
+        livestream : 'http://www.deutschlandradio.de/streaming/dradiowissen.m3u',
+        livemediathek : 'http://srv.deutschlandradio.de/aodlistaudio.1706.de.rpc?drau:station_id=1&drau:from=_DATE_&drau:to=_DATE_&drau:page=1&drau:limit=500'
     }
 };
 
