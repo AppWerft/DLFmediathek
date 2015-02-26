ecb92b06cc40e89cf0470481e227831dcd6bf163
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2015-02-26 12:49:33 +0100

          

--- Resources/model/stations.js
+++ Resources/model/stations.js
@@ -1,7 +1,8 @@
 module.exports = {
     dlf : {
         color : '#006AB3',
-        /* für Tagesübersicht, wird auch in der Mediathek genutzt */
+        /* für Tagesübersicht, wird auch in der Mediathek genutzt und einaml täglich über
+         einen Hintergrundprozess gesynct*/
         rss : 'http://www.deutschlandfunk.de/programmvorschau.281.de.rss',
         /* für LiveRadio, eigentlicher Stream wird ermittelt und gespeichert*/
         stream : 'http://www.dradio.de/streaming/dlf.m3u',
@@ -9,9 +10,9 @@ module.exports = {
         podcasts : 'http://srv.deutschlandradio.de/aodlistaudio.1706.de.rpc?drau:station_id=4&drau:from=_DATE_&drau:to=_DATE_&drau:page=1&drau:limit=500'
     },
     drk : {
+        color : '#E95D0F',
         hoerkunst : 'http://www.deutschlandradiokultur.de/hoerkunst.1656.de.html',
         rss: 'http://www.deutschlandradiokultur.de/programmvorschau.282.de.rss',
-        color : '#E95D0F',
         stream : 'http://www.dradio.de/streaming/dkultur.m3u',
         podcasts : 'http://srv.deutschlandradio.de/aodlistaudio.1706.de.rpc?drau:station_id=3&drau:from=_DATE_&drau:to=_DATE_&drau:page=1&drau:limit=500'
     },
