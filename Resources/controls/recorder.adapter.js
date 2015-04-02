2962ff16df3a509aaddc3febb0b0338720fde462
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2015-04-02 16:15:52 +0200

         

--- Resources/controls/recorder.adapter.js
+++ Resources/controls/recorder.adapter.js
@@ -5,12 +5,13 @@
  duration
 
  **/
+const BLOCKSIZE = 1024;
 
 module.exports = function(_args) {
     if (!_args.url)
         _args.url = URL;
     if (!_args.duration)
-        _args.duration = 10000;
+        _args.duration = 3000;
     var f = Ti.Filesystem.getFile((Ti.Filesystem.isExternalStoragePresent()) ? Ti.Filesystem.externalStorageDirectory : Ti.Filesystem.applicationDataDirectory, 'test.mp3');
     var regex = /^(?:([^\:]*)\:\/\/)?(?:([^\:\@]*)(?:\:([^\@]*))?\@)?(?:([^\/\:]*)\.(?=[^\.\/\:]*\.[^\.\/\:]*))?([^\.\/\:]*)(?:\.([^\/\.\:]*))?(?:\:([0-9]*))?(\/[^\?#]*(?=.*?\/)\/)?([^\?#]*)?(?:\?([^#]*))?(?:#(.*))?/;
     var res = _args.url.match(regex);
@@ -21,57 +22,54 @@ module.exports = function(_args) {
         host : _args.host,
         port : _args.port,
         connected : function(e) {
-            Ti.Stream.pump(e.socket, readCallback, 1024, true);
-            Ti.Stream.write(socket, Ti.createBuffer({
+            Ti.Stream.pump(e.socket, function(_read) {
+                if (_read.bytesProcessed == -1) {
+                    console.log('bytesProcessed == -1');
+                    console.log(_read);
+                }
+                try {
+                    if (_read.buffer) {
+                        var instream = Ti.Stream.createStream({
+                            mode : Ti.Stream.MODE_READ,
+                            source : _read.buffer
+                        });
+                        var outstream = f.open(Ti.Filesystem.MODE_WRITE);
+                        var buffer = Ti.createBuffer({
+                            length : BLOCKSIZE
+                        });
+                        var read_bytes = 0;
+                        while (( read_bytes = instream.read(buffer)) > 0) {
+                            console.log('read_bytes: ' + read_bytes);
+                            outstream.write(buffer, 0, read_bytes);
+                        }
+                        instream.close();
+                        outstream.close();
+                    } else {
+                        Ti.API.error('Error: read callback called with no buffer!');
+                    }
+                } catch (ex) {
+                    console.log('excepetzion');
+                    Ti.API.error(ex);
+                }
+            }, BLOCKSIZE, true);
+            Ti.Stream.write(e.socket, Ti.createBuffer({
                 value : 'GET ' + _args.path + ' HTTP/1.1\r\n\r\n'
-            }), writeCallback);
+            }), function(_write) {
+                Ti.API.info('Successfully wrote GET request to shoutcast server');
+            });
+
         },
         error : function(e) {
             Ti.API.info('Error (' + e.errorCode + '): ' + e.error);
         },
     });
     socket.connect();
-    var instream,
-        outstream;
-    function writeCallback(e) {
-        Ti.API.info('Successfully wrote GET request to shoutcast server');
-    }
 
-    function readCallback(e) {
-        if (e.bytesProcessed == -1) {
-            console.log(e);
-            setTimeout(function() {
-                socket && socket.close();
-                instream && instream.close();
-                outstream && outstream.close();
-            }, _args.duration);
-        }
-        try {
-
-            if (e.buffer) {
-                instream = Ti.Stream.createStream({
-                    mode : Ti.Stream.MODE_READ,
-                    source : e.buffer
-                });
-                outstream = f.open(Ti.Filesystem.MODE_WRITE);
-                var buffer = Ti.createBuffer({
-                    length : 1024
-                });
-                var read_bytes = 0;
-                while (( read_bytes = instream.read(buffer)) > 0) {
-                    console.log(read_bytes);
-                    outstream.write(buffer, 0, read_bytes);
-                }
-                instream.close();
-                outstream.close();
-            } else {
-                Ti.API.error('Error: read callback called with no buffer!');
-            }
-            // hier blockweise lesen und speichern und irgendwann beenden, aber wie?
-        } catch (ex) {
-            Ti.API.error(ex);
-        }
-    }
+    setTimeout(function() {
+        !!socket && socket.close();
+        console.log('size=' + f.getSize());
+        console.log(f.spaceAvailable());
+    }, _args.duration);
 
 };
 
