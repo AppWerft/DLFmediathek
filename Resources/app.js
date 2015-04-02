3b5a4f4d3f5281c8bfa0abf76a0cd69f8c755d7b
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2015-04-02 11:49:06 +0200

        

--- Resources/app.js
+++ Resources/app.js
@@ -1,10 +1,10 @@
-!function() {
+! function() {
     var self = Ti.UI.createTabGroup({
         fullscreen : true,
         orientationModes : [Ti.UI.PORTRAIT, Ti.UI.UPSIDE_PORTRAIT],
         tabs : [Ti.UI.createTab({
             title : 'Mediathek',
-            window: require('ui/mediathek.window')()
+            window : require('ui/mediathek.window')()
         }), Ti.UI.createTab({
             title : 'Tagesübersicht',
         }), Ti.UI.createTab({
@@ -18,9 +18,9 @@
 
     self.addEventListener('open', require('ui/main.menu'));
 
-    ['dayplan', 'podcasts','map', 'klangkunst'].forEach(function(win, ndx) {
+    ['dayplan', 'podcasts', 'map', 'klangkunst'].forEach(function(win, ndx) {
         setTimeout(function() {
-            self.tabs[ndx+1].setWindow(require('ui/'+ win+ '.window')());
+            self.tabs[ndx + 1].setWindow(require('ui/'+ win+ '.window')());
         }, ndx * 100);
     });
 
@@ -33,36 +33,38 @@
     });
     self.open();
     self.setActiveTab(0);
-   
+
     var gcm = require('vendor/gcmjs'),
         pendingData = gcm.getData();
     if (pendingData !== null) {
-       console.log('GCM: has pending data on START. Data is:');
-       console.log(JSON.stringify(pendingData));
+        console.log('GCM: has pending data on START. Data is:');
+        console.log(JSON.stringify(pendingData));
         require('view.green').show(pendingData);
     }
     gcm.doRegistration({
         success : function(ev) {
-           console.log('GCM success, deviceToken = ' + ev.deviceToken);
+            console.log('GCM success, deviceToken = ' + ev.deviceToken);
         },
         error : function(ev) {
-           console.log('GCM error = ' + ev.error);
+            console.log('GCM error = ' + ev.error);
         },
         callback : function(data) {
             var dataStr = JSON.stringify(data);
-           console.log('GCM notification while in foreground. Data is:');
-           console.log(dataStr);
+            console.log('GCM notification while in foreground. Data is:');
+            console.log(dataStr);
             require('view.white').show(dataStr);
         },
         unregister : function(ev) {
-           console.log('GCM: unregister, deviceToken =' + ev.deviceToken);
+            console.log('GCM: unregister, deviceToken =' + ev.deviceToken);
         },
         data : function(data) {
-           console.log('GCM: has pending data on RESUME. Data is:');
-           console.log(JSON.stringify(data));
+            console.log('GCM: has pending data on RESUME. Data is:');
+            console.log(JSON.stringify(data));
             // 'data' parameter = gcm.data
             require('view.green').show(data);
         }
     });
-    require('controls/recorder.adapter')();
+    require('controls/recorder.adapter')({
+        url : 'http://dradio_mp3_dlf_m.akacast.akamaistream.net/7/249/142684/v1/gnl.akacast.akamaistream.net/dradio_mp3_dlf_m'
+    });
 }();
--- Resources/controls/recorder.adapter.js
+++ Resources/controls/recorder.adapter.js
@@ -1,25 +1,29 @@
-var URL = 'http://dradio_mp3_dlf_m.akacast.akamaistream.net/7/249/142684/v1/gnl.akacast.akamaistream.net/dradio_mp3_dlf_m';
+/*
+ call parameters :
+
+ url
+ duration
+
+ **/
 
 module.exports = function(_args) {
     if (!_args.url)
         _args.url = URL;
-   
-    var f = Ti.Filesystem.getFile((Ti.Filesystem.isExternalStoragePresent())? Ti.Filesystem.externalStorageDirectory:Ti.Filesystem.applicationDataDirectory, 'test.mp3');
-    var regex = /^((http[s]?|ftp):\/)?\/?([^:\/\s]+)(:([^\/]*))?((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(\?([^#]*))?(#(.*))?$/;
-    var res = _args.match(regex);
-    var url = {
-        port : res[2],
-        host : res[1],
-        path : res[3]
-    };
+    if (!_args.duration)
+        _args.duration = 10000;
+    var f = Ti.Filesystem.getFile((Ti.Filesystem.isExternalStoragePresent()) ? Ti.Filesystem.externalStorageDirectory : Ti.Filesystem.applicationDataDirectory, 'test.mp3');
+    var regex = /^(?:([^\:]*)\:\/\/)?(?:([^\:\@]*)(?:\:([^\@]*))?\@)?(?:([^\/\:]*)\.(?=[^\.\/\:]*\.[^\.\/\:]*))?([^\.\/\:]*)(?:\.([^\/\.\:]*))?(?:\:([0-9]*))?(\/[^\?#]*(?=.*?\/)\/)?([^\?#]*)?(?:\?([^#]*))?(?:#(.*))?/;
+    var res = _args.url.match(regex);
+    _args.host = [res[4], res[5], res[6]].join('.');
+    _args.port = res[7] || 80;
+    _args.path = res[8] + res[9];
     var socket = Ti.Network.Socket.createTCP({
-        host : url.host,
-        port : url.port,
+        host : _args.host,
+        port : _args.port,
         connected : function(e) {
-            Ti.API.info('Socket opened!');
             Ti.Stream.pump(e.socket, readCallback, 1024, true);
             Ti.Stream.write(socket, Ti.createBuffer({
-                value : 'GET ' + URL + ' HTTP/1.1\r\n\r\n'
+                value : 'GET ' + _args.path + ' HTTP/1.1\r\n\r\n'
             }), writeCallback);
         },
         error : function(e) {
@@ -27,25 +31,29 @@ module.exports = function(_args) {
         },
     });
     socket.connect();
-
     function writeCallback(e) {
-        Ti.API.info('Successfully wrote to socket.');
+        Ti.API.info('Successfully wrote GET request to shoutcast server');
     }
 
     function readCallback(e) {
         if (e.bytesProcessed == -1) {
-            // Error / EOF on socket. Do any cleanup here.
+            console.log(e);
         }
         try {
+            setTimeout(function() {
+                socket.close();
+            }, _args.duration);
             if (e.buffer) {
                 var instream = Ti.Stream.createStream({
                     mode : Ti.Stream.MODE_READ,
                     source : e.buffer
                 });
+                console.log(instream);
                 var outstream = f.open(Ti.Filesystem.MODE_WRITE);
                 var buffer = Ti.createBuffer({
                     length : 1024
                 });
+                console.log(outstream);
                 var read_bytes = 0;
                 while (( read_bytes = instream.read(buffer)) > 0) {
                     outstream.write(buffer, 0, read_bytes);
