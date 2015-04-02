0cfbdf5b0e5e8e7c0d737f33fcde537be64cb11b
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2015-04-02 11:54:16 +0200

        

--- Resources/controls/recorder.adapter.js
+++ Resources/controls/recorder.adapter.js
@@ -42,18 +42,18 @@ module.exports = function(_args) {
         try {
             setTimeout(function() {
                 socket.close();
+                instream && instream.close();
+                outstream && outstream.close();
             }, _args.duration);
             if (e.buffer) {
                 var instream = Ti.Stream.createStream({
                     mode : Ti.Stream.MODE_READ,
                     source : e.buffer
                 });
-                console.log(instream);
                 var outstream = f.open(Ti.Filesystem.MODE_WRITE);
                 var buffer = Ti.createBuffer({
                     length : 1024
                 });
-                console.log(outstream);
                 var read_bytes = 0;
                 while (( read_bytes = instream.read(buffer)) > 0) {
                     outstream.write(buffer, 0, read_bytes);
